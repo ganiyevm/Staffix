b490c81bea3689ca3db235f645365334a30e6ebb
Hiramoo <hiramoo@Hiramoos-MacBook-Pro.local>
2025-02-11 21:08:41 +0500

    loglar bilan ishladim

--- logs/logger.js
+++ logs/logger.js
@@ -1,13 +1,12 @@
 const winston = require("winston");
 const path = require("path");
 
-// Faqat info va warn darajadagi loglarni qabul qiluvchi filter
 const infoAndWarnFilter = winston.format((info, opts) => {
   return info.level === "info" || info.level === "warn" ? info : false;
 })();
 
 const logger = winston.createLogger({
-  level: "info", // Umumiy log darajasi
+  level: "info", 
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.printf(({ timestamp, level, message }) => {
@@ -15,12 +14,10 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
-    // Faqat info va warn loglarini yozish
     new winston.transports.File({
       filename: path.join(__dirname, "../logs/app.log"),
       format: winston.format.combine(infoAndWarnFilter),
     }),
-    // Faqat error loglarini yozish
     new winston.transports.File({
       filename: path.join(__dirname, "../logs/error.log"),
       level: "error",
